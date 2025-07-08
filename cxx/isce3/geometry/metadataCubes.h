b38758ba6abb80c17726941e775d0657a0599ad4
Gustavo H. X. Shiroma <52007211+gshiroma@users.noreply.github.com>
2025-07-08 12:08:50 -0700

    Add option to compute the ground-track velocity using rdr2geo (radar grid cubes) (#38)
    * Add option to compute the ground-track velocity using rdr2geo (radar grid cubes)
    
    * improve docstrings for flag flag_ground_velocity_from_rdr2geo

--- cxx/isce3/geometry/metadataCubes.h
+++ cxx/isce3/geometry/metadataCubes.h
@@ -144,6 +144,11 @@ void writeVectorDerivedCubes(const int array_pos_i,
  * derivative of doppler
  * @param[in]  flag_set_output_rasters_geolocation Set output rasters'
  * geotransform and spatial reference
+ * @param[in] flag_ground_velocity_from_rdr2geo When true, compute ground-track
+ * velocity using a finite-difference approximation between grid locations in
+ * the azimuth direction (`rdr2geo` method). In this case, the azimuth spacing
+ * of the grid affects the accuracy of the approximation. When false, use a
+ * closed-form expression for a geocentric spherical surface model instead.
  */
 void makeRadarGridCubes(const isce3::product::RadarGridParameters& radar_grid,
         const isce3::product::GeoGridParameters& geogrid,
@@ -161,7 +166,8 @@ void makeRadarGridCubes(const isce3::product::RadarGridParameters& radar_grid,
         isce3::io::Raster* ground_track_velocity_raster = nullptr,
         const double threshold_geo2rdr = 1e-8, const int numiter_geo2rdr = 100,
         const double delta_range = 1e-8,
-        bool flag_set_output_rasters_geolocation = false);
+        bool flag_set_output_rasters_geolocation = false,
+        const bool flag_ground_velocity_from_rdr2geo = true);
 
 /** Make metadata geolocation grid cubes
  *
--- python/extensions/pybind_isce3/geometry/metadataCubes.cpp
+++ python/extensions/pybind_isce3/geometry/metadataCubes.cpp
@@ -30,6 +30,7 @@ void addbinding_metadata_cubes(py::module & m)
             py::arg("numiter_geo2rdr") = defaults.maxiter,
             py::arg("delta_range") = defaults.delta_range,
             py::arg("flag_set_output_rasters_geolocation") = false,
+            py::arg("flag_ground_velocity_from_rdr2geo") = true,
             R"(Make metadata radar grid cubes
 
                Metadata radar grid cubes describe the radar geometry
@@ -96,8 +97,14 @@ void addbinding_metadata_cubes(py::module & m)
                     Step size used for computing derivative of doppler
                 flag_set_output_rasters_geolocation : bool
                     Set output rasters' geotransform and spatial reference
-
-)");
+                flag_ground_velocity_from_rdr2geo, bool, optional
+                    When True, compute ground-track velocity using a
+                    finite-difference approximation between grid locations in
+                    the azimuth direction (`rdr2geo` method). In this case, the
+                    azimuth spacing of the grid affects the accuracy of the
+                    approximation. When False, use a closed-form expression for
+                    a geocentric spherical surface model instead.
+                    )");
 
     m.def("make_geolocation_cubes", &isce3::geometry::makeGeolocationGridCubes,
             py::arg("radar_grid"), py::arg("heights"), py::arg("orbit"),
